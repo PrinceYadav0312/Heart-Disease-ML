6bdd9459824f4a206eb25e28b198ee60ee0c40b6
PrinceYadav0312 <princekumaryadav686@gmail.com>
2025-12-09 14:19:02 +0530

    Creating Frontend

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import joblib
 
-model = joblib.load()
+model = joblib.load('Logistic_Heart.pkl')
+scaler = joblib.load('scaker.pkl')
+expected_columns = joblib.load('columns.pkl')
+
+st.title("heart strpke prediction by Prince")
+st.markdown('Provide the following details')
+
+age = st.slider('Age', 18, 100, 40)
